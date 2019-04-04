21da305dd5c72ca5661fbb0ce5a7b8e494e8ed59
Liran Harary <liran.harary@chargeafter.com>
2019-04-04 14:22:04 +0300

    Lesson 21 - selectors

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // We can also select by class and by attribute, but we normally just select by tag name
   selector: 'app-root',
   templateUrl: './app.component.html',
   // styleUrls: ['./app.component.css']
