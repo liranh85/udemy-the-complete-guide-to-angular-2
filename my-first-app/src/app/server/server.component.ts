5bd076487c9978a8cd4119c0d8e5232957cabd0b
Liran Harary <liran.harary@chargeafter.com>
2019-04-04 16:31:16 +0300

    Lesson 23 - Databinding: String Interpolation

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -5,5 +5,7 @@ import { Component } from '@angular/core';
     templateUrl: './server.component.html'
 })
 export class ServerComponent {
-
+    // The added types are not required in TypeScript, as they can be inferred from the value. We added them for demo purposes only
+    serverId: number = 10;
+    serverStatus: string = 'offline';
 }
