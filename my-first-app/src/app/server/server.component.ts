955a77382ae9f98b470594517eb8ea39a625ed14
Liran Harary <liran.harary@chargeafter.com>
2019-04-08 13:01:01 +0300

    Lesson 35 - Styling Elements Dynamically with ngStyle

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -8,4 +8,12 @@ export class ServerComponent {
     // The added types are not required in TypeScript, as they can be inferred from the value. We added them for demo purposes only
     serverId: number = 10;
     serverStatus: string = 'offline';
+
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline'
+    }
+
+    getColor() {
+        return this.serverStatus === 'online' ? 'green' : 'red';
+    }
 }
