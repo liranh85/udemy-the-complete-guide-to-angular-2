bf1d9abdf3ee2f5d81841c4f5b65fb0057606b59
Liran Harary <liran.harary@chargeafter.com>
2019-05-12 11:43:21 +0300

    Up to 51 (inclusive)

--- food-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ food-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Recipe_logo.jpeg/600px-Recipe_logo.jpeg'),
   ];
 
   constructor() { }
