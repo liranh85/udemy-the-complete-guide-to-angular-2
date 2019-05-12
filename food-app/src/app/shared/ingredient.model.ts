d15a675aab702c20f937f8be5140de209efb4e6c
Liran Harary <liran.harary@chargeafter.com>
2019-05-12 12:11:11 +0300

    Up to 57 (inclusive)

---
+++ food-app/src/app/shared/ingredient.model.ts
@@ -0,0 +1,12 @@
+export class Ingredient {
+  // public name: string;
+  // public amount: number;
+
+  // constructor(name: string, amount: number) {
+  //   this.name = name;
+  //   this.amount = amount;
+  // }
+
+  // This achieves the above in one line of code
+  constructor(public name: string, public amount: number) {}
+}
