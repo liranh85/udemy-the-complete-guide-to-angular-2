438fb4391ccadc3b34a15d8cb48e6b6baed186a6
Liran Harary <liran.harary@chargeafter.com>
2019-05-28 10:46:33 +0300

    Lesson 72: Using Local References in Templates

--- session5-servers-project/src/app/cockpit/cockpit.component.ts
+++ session5-servers-project/src/app/cockpit/cockpit.component.ts
@@ -9,7 +9,8 @@ export class CockpitComponent implements OnInit {
   // We need the Output() decorator to make it listenable from outside
   @Output('sCreated') serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();;
-  newServerName = '';
+  // No longer needed now that we're using a local reference in the template and thus are able to pass the input to the method, to read its value.
+  // newServerName = '';
   newServerContent = '';
 
   constructor() { }
@@ -17,16 +18,16 @@ export class CockpitComponent implements OnInit {
   ngOnInit() {
   }
 
-  onAddServer() {
+  onAddServer(nameInput) {
     this.serverCreated.emit({
-      serverName: this.newServerName,
+      serverName: nameInput.value,
       serverContent: this.newServerContent
     });
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(nameInput) {
     this.blueprintCreated.emit({
-      serverName: this.newServerName,
+      serverName: nameInput.value,
       serverContent: this.newServerContent
     });
   }
