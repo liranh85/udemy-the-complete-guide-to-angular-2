baef859d160e370db699b6f57389ca9b8e2b1c17
Liran Harary <liran.harary@chargeafter.com>
2019-05-28 10:36:56 +0300

    Component View Encapsulation options

--- session5-servers-project/src/app/server-element/server-element.component.ts
+++ session5-servers-project/src/app/server-element/server-element.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, /*ViewEncapsulation*/ } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  // encapsulation: ViewEncapsulation.Native, // Use Shadow DOM for CSS encapsulation
+  // encapsulation: ViewEncapsulation.None, // Don't encapsulate styles, e.g. `p { color: red }` will affect all `<p>` in the document
+  // encapsulation: ViewEncapsulation.Emulated, // Default behaviour, encapsulate styles by emulating the Shadow DOM - by adding custom attributes to the elements, and selecting those in the CSS.
 })
 export class ServerElementComponent implements OnInit {
   // We need the Input() decorator to expose this property, so that any parent component can bind to it
