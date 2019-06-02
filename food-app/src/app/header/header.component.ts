b8bbcb552fda23584d64643b76bcd2a4dd153157
Liran Harary <liran.harary@chargeafter.com>
2019-06-02 16:32:05 +0300

    Lesson 84: my attempt at simple navigation

--- food-app/src/app/header/header.component.ts
+++ food-app/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @Output() navClicked = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onClickNav($event) {
+    $event.stopPropagation();
+    const { name } = $event.target
+    console.log(name);
+    this.navClicked.emit(name)
   }
 
 }
